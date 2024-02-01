967cfa6b46732673204cf848851f1282cc1b1038
TDMwangi <teddymwangi65@gmail.com>
2024-02-01 14:00:38 +0300

    feat: add ami data block

--- main.tf
+++ main.tf
@@ -5,3 +5,19 @@ provider "aws" {
 # Retrieve the list of availability zones in the region
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
