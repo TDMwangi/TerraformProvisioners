18dd61147017e6d342f1a1c84ff2d4564452fe34
TDMwangi <teddymwangi65@gmail.com>
2024-02-04 06:27:09 +0300

    feat: generate an AWS key pair

--- main.tf
+++ main.tf
@@ -32,3 +32,13 @@ resource "local_file" "private_key_pem" {
   content  = tls_private_key.generated.private_key_pem
   filename = "MyAWSKey.pem"
 }
+
+# Create a new key pair
+resource "aws_key_pair" "generated" {
+  key_name   = "teddy_key_pair"
+  public_key = tls_private_key.generated.public_key_openssh
+
+  lifecycle {
+    ignore_changes = [key_name]
+  }
+}
